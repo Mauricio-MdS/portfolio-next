d87a93273196047c41225bfdc44972b35411a711
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2023-08-05 09:22:55 -0300

    fix ClickableCard tabindex

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -22,7 +22,6 @@ const ClickableCard = React.forwardRef<
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
   <div
-    tabIndex={0}
     ref={ref}
     className={cn(
       "rounded-lg border bg-card text-card-foreground shadow-sm hover:bg-accent hover:text-accent-foreground transition-colors cursor-pointer",

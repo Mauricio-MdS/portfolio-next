29761746ae2ba8d6d6e823455a97d2d39f7b8139
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2023-08-08 14:29:56 -0300

    update about text

--- components/about/about.tsx
+++ components/about/about.tsx
@@ -5,10 +5,10 @@ export default function About() {
     <section className="container">
       <TypographyH2>About</TypographyH2>
       <TypographyP>
-        In the early 2000s, I took my first steps into the world of the web by
-        creating a website that showcased the local bands in the coastal scene
-        of Sao Paulo. Skip ahead to today, and I&#39;m having a lot of fun
-        building some seriously awesome content for the web.
+        In the early 2000s, I took my first steps into the world of web
+        development by creating a website that showcased the local bands in the
+        coastal scene of Sao Paulo. Skip ahead to today, and I&#39;m having a
+        lot of fun building some seriously awesome content for the web.
       </TypographyP>
       <TypographyP>
         My current focus revolves around crafting value for{" "}
@@ -20,8 +20,8 @@ export default function About() {
         has led me to exciting collaborations on significant projects. As a
         coding enthusiast, I&#39;m all in when it comes to tackling challenges
         on both the front and back ends. Lately, I&#39;ve been diving into the
-        world of Next.js, an interesting amagalm of React and Node.js that&#39;s
-        a paradise for a full stack dev.
+        world of Next.js, an interesting mix of React and Node.js that&#39;s a
+        paradise for a full stack dev.
       </TypographyP>
       <TypographyP>
         However, life isn&#39;t solely about code and pixels! When I&#39;m not
